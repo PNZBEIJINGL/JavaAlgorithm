160f28b6253d191a3fe353dc2ef3fb8b3b389e74
PNZBEIJINGL <63351525@qq.com>
2019-12-24 23:04:18 +0800

    1056

---
+++ src/main/java/com/algorithm/number/NumAdd_1056.java
@@ -0,0 +1,23 @@
+package com.algorithm.number;
+
+import java.util.Scanner;
+
+public class NumAdd_1056 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int [n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(j != i)
+                    ans += arr[i]*10 + arr[j];
+            }
+        }
+        System.out.println(ans);
+    }
+
+}
