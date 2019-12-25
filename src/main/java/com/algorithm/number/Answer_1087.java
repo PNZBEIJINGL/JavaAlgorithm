bbd56061a88f7e80ad2e31f05820756b1a199749
PNZBEIJINGL <63351525@qq.com>
2019-12-26 00:43:02 +0800

    1087

---
+++ src/main/java/com/algorithm/number/Answer_1087.java
@@ -0,0 +1,18 @@
+package com.algorithm.number;
+
+import java.util.Scanner;
+
+public class Answer_1087 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+        int result = N;
+        for (int i = 2; i <= N; ++i) {						// 从2开始
+            if (0 != i % 2 && 0 != i % 3 && 0 != i % 5) {	// 不是2,3,5的倍数
+                result -= 1;									// 个数减1
+            }
+        }
+        System.out.println(result);
+    }
+}
