c750220f4299ce5e898b141a4fdc303836f0dabd
liuyong <liuyong@startimes.com.cn>
2021-09-06 18:33:58 +0800

    JVM GC 2109061833

---
+++ src/main/java/com/jvm/gc/ReferenceConuntingGC.java
@@ -0,0 +1,28 @@
+package com.jvm.gc;
+
+/**
+ * 第二章：引用计数算法的缺陷
+ * 引用计数算法（Reference Counting）给对象中添加一个引用计数器，每当有一个地方引用它时，计数器值就加1；当引用失效时，计数器值就减1；任何时刻计数器为0的对象就是不可能再被使用的
+ */
+public class ReferenceConuntingGC {
+    public Object instance = null;
+    private static final int int1MB = 1024 * 1024;
+    /**
+     * 这个成员属性的唯一意义就是占点内存，以便能在GC日志中看清楚是否被回收过
+     */
+    private byte[] bigSize = new byte[2 * int1MB];
+
+    public static void main(String[] args) {
+
+
+        ReferenceConuntingGC objA = new ReferenceConuntingGC();
+        ReferenceConuntingGC objB = new ReferenceConuntingGC();
+        objA.instance = objB;
+        objB.instance = objA;
+        objA = null;
+        objB = null;
+        //假设在这行发生GC,objA和objB是否能被回收？
+        System.gc();
+    }
+
+}
