46e293c797e2af40b02fa3b7664abcf4c4e9ce02
liuyong <liuyong@startimes.com.cn>
2022-03-09 18:07:59 +0800

    PAT

--- src/main/java/com/pat/basiclevel/Q1019.java
+++ src/main/java/com/pat/basiclevel/Q1019.java
@@ -1,4 +1,4 @@
-package com.algorithm.pat.basiclevel;
+package com.pat.basiclevel;
 
 import java.util.Arrays;
 import java.util.Scanner;
---
+++ src/main/java/com/pat/basiclevel/package-info.java
@@ -0,0 +1,4 @@
+package com.pat.basiclevel;
+
+//https://pintia.cn/problem-sets/994805260223102976/problems/type/7
+//PAT (Basic Level) Practice （中文）
