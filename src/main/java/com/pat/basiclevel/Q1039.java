1915b517f9cbb10ba174f06e98ce79619a76557c
liuyong <liuyong@startimes.com.cn>
2022-03-11 11:26:00 +0800

    PAT:1039

---
+++ src/main/java/com/pat/basiclevel/Q1039.java
@@ -0,0 +1,50 @@
+package com.pat.basiclevel;
+
+import java.util.Scanner;
+
+/**
+ * 1039 到底买不买 (20 分)
+ */
+public class Q1039 {
+    //ppRYYGrrYBR2258
+    //YrR8RrY
+    //Yes 8
+    //ppRYYGrrYB225
+    //YrR8RrY
+    //No 2
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        char[] all = s.next().toCharArray();
+        char[] need = s.next().toCharArray();
+
+        //一定长度的数据ASC吗为数组下标,数据内容是字符穿线次数
+        int[] allASCII = new int[96 + 26 + 1];
+        int[] needASCII = new int[96 + 26 + 1];
+        for (int i = 0; i < all.length; i++) {
+            allASCII[Integer.valueOf(all[i])]++;
+        }
+        for (int i = 0; i < need.length; i++) {
+            needASCII[Integer.valueOf(need[i])]++;
+        }
+
+        //判断是否足够
+        int num = 0;
+        boolean enough = true;
+        for (int i = 0; i < needASCII.length; i++) {
+            if (needASCII[i] != 0) {
+                if (needASCII[i] > allASCII[i]) {
+                    num = num + (needASCII[i] - allASCII[i]);
+                    enough = false;
+                }
+            }
+        }
+        if (enough) {
+            //足够时，差值为多的数量
+            System.out.println("Yes " + (all.length - need.length));
+        } else {
+            //不足时，使用累计的数量
+            System.out.println("No " + num);
+        }
+    }
+}
