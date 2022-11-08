907a6b11693896f207002e0b79e2568b5313f132
liuyong <liuyong@startimes.com.cn>
2022-11-08 14:39:43 +0800

    timer

---
+++ src/main/java/com/java6/TimerTest.java
@@ -0,0 +1,36 @@
+package com.java6;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+public class TimerTest {
+    private static int delay = 1000;
+    private static int initialDelay = 5000;
+    private static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ActionListener listener = new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.println("count = " + count++);
+            }
+        };
+
+        Timer timer = new Timer(delay, listener);
+        timer.setInitialDelay(initialDelay);
+
+        //timer.setRepeats(false);
+        timer.start();
+
+        for (; ; ) {
+            if (count == 5)
+                timer.setDelay(500);
+            if (count == 10)
+                timer.setRepeats(false);
+        }
+    }
+
+
+}
